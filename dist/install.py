c4430dc9653bb0c8567dd40c56e7e1ffc7b7b398
Sven Reifschneider <9088619+neoground@users.noreply.github.com>
2021-03-23 18:18:38 +0100

    🔧 Version change

--- dist/install.py
+++ dist/install.py
@@ -8,7 +8,7 @@ def loader():
 class BasicInstaller(ExtensionInstaller):
     def __init__(self):
         super(BasicInstaller, self).__init__(
-            version="1.8",
+            version="1.9",
             name='neowx-material',
             description='The most versatile and modern weewx skin',
             author="Neoground GmbH",
